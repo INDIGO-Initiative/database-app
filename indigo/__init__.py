540c734923202c488b5f0c8f639de1fccbd20b2a
James <james.baster@opendataservices.coop>
2020-06-05 15:55:46 +0100

    organisations: link to projects; fields is now in config

--- indigo/__init__.py
+++ indigo/__init__.py
@@ -4,7 +4,16 @@ TYPE_ORGANISATION_PUBLIC_ID = "organisation"
 JSONDATAFERRET_HOOKS = "indigo.jsondataferret"
 
 TYPE_PROJECT_ALWAYS_FILTER_KEYS_LIST = ["/status"]
-
 TYPE_PROJECT_FILTER_KEYS_LIST = ["/project_name", "/fund_name", "/launch_date"]
+TYPE_PROJECT_ORGANISATION_LISTS_LIST = [
+    {
+        "list_key": "/outcome_funds",
+        "item_id_key": "/organisation/id",
+        "item_to_org_map": {
+            "/organisation/name": "/name",
+            "/organisation/type": "/type",
+        },
+    }
+]
 
 TYPE_ORGANISATION_ALWAYS_FILTER_KEYS_LIST = ["/contact"]
--- indigo/processdata.py
+++ indigo/processdata.py
@@ -3,41 +3,44 @@ from collections import defaultdict
 import jsondataferret
 import jsonpointer
 
-from indigo import TYPE_ORGANISATION_PUBLIC_ID, TYPE_PROJECT_PUBLIC_ID
+from indigo import (
+    TYPE_ORGANISATION_PUBLIC_ID,
+    TYPE_PROJECT_ORGANISATION_LISTS_LIST,
+    TYPE_PROJECT_PUBLIC_ID,
+)
 from indigo.models import Organisation
 
 
 def add_other_records_to_project(input_json, public_only=False):
-    data_list = jsonpointer.resolve_pointer(input_json, "/outcome_funds", default=None)
-    if isinstance(data_list, list) and data_list:
-        for data_item in data_list:
-            org_id = jsonpointer.resolve_pointer(
-                data_item, "/organisation/id", default=None
-            )
-            if org_id:
-                try:
-                    organisation = Organisation.objects.get(public_id=org_id)
-                    organisation_data = (
-                        organisation.data_public
-                        if public_only
-                        else organisation.data_private
-                    )
-                    jsonpointer.set_pointer(
-                        data_item,
-                        "/organisation/name",
-                        jsonpointer.resolve_pointer(
-                            organisation_data, "/name", default=None
-                        ),
-                    )
-                    jsonpointer.set_pointer(
-                        data_item,
-                        "/organisation/type",
-                        jsonpointer.resolve_pointer(
-                            organisation_data, "/type", default=None
-                        ),
-                    )
-                except Organisation.DoesNotExist:
-                    pass
+    for organisation_list_data in TYPE_PROJECT_ORGANISATION_LISTS_LIST:
+        data_list = jsonpointer.resolve_pointer(
+            input_json, organisation_list_data["list_key"], default=None
+        )
+        if isinstance(data_list, list) and data_list:
+            for data_item in data_list:
+                org_id = jsonpointer.resolve_pointer(
+                    data_item, organisation_list_data["item_id_key"], default=None
+                )
+                if org_id:
+                    try:
+                        organisation = Organisation.objects.get(public_id=org_id)
+                        organisation_data = (
+                            organisation.data_public
+                            if public_only
+                            else organisation.data_private
+                        )
+                        for data_key, org_key in organisation_list_data[
+                            "item_to_org_map"
+                        ].items():
+                            jsonpointer.set_pointer(
+                                data_item,
+                                data_key,
+                                jsonpointer.resolve_pointer(
+                                    organisation_data, org_key, default=None
+                                ),
+                            )
+                    except Organisation.DoesNotExist:
+                        pass
     return input_json
 
 
@@ -45,32 +48,35 @@ def extract_edits_from_project_import(record, import_json):
     out = []
     organisation_data = defaultdict(list)
 
-    data_list = jsonpointer.resolve_pointer(import_json, "/outcome_funds", default=None)
-    if isinstance(data_list, list) and data_list:
-        for data_item in data_list:
-            # Extract Org data
-            org_id = jsonpointer.resolve_pointer(
-                data_item, "/organisation/id", default=None
-            )
-            if org_id:
-                organisation_data[org_id].append(
-                    {
-                        "name": jsonpointer.resolve_pointer(
-                            data_item, "/organisation/name", default=None
-                        ),
-                        "type": jsonpointer.resolve_pointer(
-                            data_item, "/organisation/type", default=None
-                        ),
-                    }
+    ################### Look For Orgs
+    for organisation_list_data in TYPE_PROJECT_ORGANISATION_LISTS_LIST:
+        data_list = jsonpointer.resolve_pointer(
+            import_json, organisation_list_data["list_key"], default=None
+        )
+        if isinstance(data_list, list) and data_list:
+            for data_item in data_list:
+                # Extract Org data
+                org_id = jsonpointer.resolve_pointer(
+                    data_item, organisation_list_data["item_id_key"], default=None
                 )
-            # Finally remove org attributes
-            jsonpointer.set_pointer(
-                data_item, "/organisation/name", None,
-            )
-            jsonpointer.set_pointer(
-                data_item, "/organisation/type", None,
-            )
+                if org_id:
+                    org_data = {}
+                    for data_key, org_key in organisation_list_data[
+                        "item_to_org_map"
+                    ].items():
+                        jsonpointer.set_pointer(
+                            org_data,
+                            org_key,
+                            jsonpointer.resolve_pointer(
+                                data_item, data_key, default=None
+                            ),
+                        )
+                        jsonpointer.set_pointer(
+                            data_item, data_key, None,
+                        )
+                    organisation_data[org_id].append(org_data)
 
+    ################### Now we have removed org data, create project edit
     out.append(
         jsondataferret.pythonapi.newevent.NewEventData(
             TYPE_PROJECT_PUBLIC_ID,
@@ -80,6 +86,7 @@ def extract_edits_from_project_import(record, import_json):
         )
     )
 
+    ################### Create organisation edits
     for org_id, org_datas in organisation_data.items():
         try:
             organisation = Organisation.objects.get(public_id=org_id)
