2c4e1061e03c804c5e3a27ebf55d8d0c0d4cbd65
James <james.baster@opendataservices.coop>
2020-06-10 14:43:12 +0100

    heroku: Setup

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -12,37 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
-import environ
-
-env = environ.Env(  # set default values and casting
-    # SECURITY WARNING: keep the secret key used in production secret!
-    JSONDATAFERRET_SECRET_KEY=(
-        str,
-        "lz@kp-&z6grz#fp#*!mi6c4-mozm)1u6m$57j%v21#u9l#lnog",
-    ),
-    JSONDATAFERRET_DEBUG=(bool, True),
-    JSONDATAFERRET_ALLOWED_HOSTS=(list, []),
-    JSONDATAFERRET_DATABASE_NAME=(str, "app"),
-    JSONDATAFERRET_DATABASE_USER=(str, "app"),
-    JSONDATAFERRET_DATABASE_PASSWORD=(str, "password"),
-    JSONDATAFERRET_DATABASE_HOST=(str, "localhost"),
-)
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("JSONDATAFERRET_SECRET_KEY")
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env("JSONDATAFERRET_DEBUG")
-
-ALLOWED_HOSTS = env("JSONDATAFERRET_ALLOWED_HOSTS")
-
 
 # Application definition
 
@@ -88,20 +59,6 @@ TEMPLATES = [
 WSGI_APPLICATION = "djangoproject.wsgi.application"
 
 
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": env("JSONDATAFERRET_DATABASE_NAME"),
-        "USER": env("JSONDATAFERRET_DATABASE_USER"),
-        "PASSWORD": env("JSONDATAFERRET_DATABASE_PASSWORD"),
-        "HOST": env("JSONDATAFERRET_DATABASE_HOST"),
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
@@ -137,7 +94,7 @@ STATIC_URL = "/static/"
 
 LOGIN_REDIRECT_URL = "/app/"
 
-# We disable MemoryFileUploadHandler - there is code in example app that assumes all files are written to disk
+# We disable MemoryFileUploadHandler - there is code in app that assumes all files are written to disk
 FILE_UPLOAD_HANDLERS = ["django.core.files.uploadhandler.TemporaryFileUploadHandler"]
 
 JSONDATAFERRET_SPREADSHEET_FORM_DATE_FORMAT = "%Y-%m-%d"
@@ -201,3 +158,47 @@ JSONDATAFERRET_TYPE_INFORMATION = {
         ],
     },
 }
+
+
+if "ON_HEROKU" in os.environ:
+    import django_heroku
+
+    django_heroku.settings(locals())
+    DEBUG = False
+
+
+else:
+
+    import environ
+
+    env = environ.Env(  # set default values and casting
+        # SECURITY WARNING: keep the secret key used in production secret!
+        JSONDATAFERRET_SECRET_KEY=(
+            str,
+            "lz@kp-&z6grz#fp#*!mi6c4-mozm)1u6m$57j%v21#u9l#lnog",
+        ),
+        JSONDATAFERRET_DEBUG=(bool, True),
+        JSONDATAFERRET_ALLOWED_HOSTS=(list, []),
+        JSONDATAFERRET_DATABASE_NAME=(str, "app"),
+        JSONDATAFERRET_DATABASE_USER=(str, "app"),
+        JSONDATAFERRET_DATABASE_PASSWORD=(str, "password"),
+        JSONDATAFERRET_DATABASE_HOST=(str, "localhost"),
+    )
+
+    # SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY = env("JSONDATAFERRET_SECRET_KEY")
+
+    # SECURITY WARNING: don't run with debug turned on in production!
+    DEBUG = env("JSONDATAFERRET_DEBUG")
+
+    ALLOWED_HOSTS = env("JSONDATAFERRET_ALLOWED_HOSTS")
+
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": env("JSONDATAFERRET_DATABASE_NAME"),
+            "USER": env("JSONDATAFERRET_DATABASE_USER"),
+            "PASSWORD": env("JSONDATAFERRET_DATABASE_PASSWORD"),
+            "HOST": env("JSONDATAFERRET_DATABASE_HOST"),
+        }
+    }
