decbf356617f159e23642bf3c40468ec63027a83
James <james.baster@opendataservices.coop>
2020-07-09 12:42:01 +0100

    web: Clearer label on history comment form fields

--- indigo/forms.py
+++ indigo/forms.py
@@ -1,10 +1,12 @@
 from django import forms
 
+COMMENT_LABEL = "Comment for history"
+
 
 class ProjectNewForm(forms.Form):
     id = forms.SlugField()
     name = forms.CharField()
-    comment = forms.CharField(widget=forms.Textarea)
+    comment = forms.CharField(widget=forms.Textarea, label=COMMENT_LABEL)
 
 
 class ProjectImportForm(forms.Form):
@@ -12,34 +14,34 @@ class ProjectImportForm(forms.Form):
 
 
 class ProjectImportStage2Form(forms.Form):
-    comment = forms.CharField(widget=forms.Textarea)
+    comment = forms.CharField(widget=forms.Textarea, label=COMMENT_LABEL)
 
 
 class ProjectMakeDisputedForm(forms.Form):
-    comment = forms.CharField(widget=forms.Textarea)
+    comment = forms.CharField(widget=forms.Textarea, label=COMMENT_LABEL)
 
 
 class ProjectMakePrivateForm(forms.Form):
-    comment = forms.CharField(widget=forms.Textarea)
+    comment = forms.CharField(widget=forms.Textarea, label=COMMENT_LABEL)
 
 
 class OrganisationNewForm(forms.Form):
     id = forms.SlugField()
     name = forms.CharField()
-    comment = forms.CharField(widget=forms.Textarea)
+    comment = forms.CharField(widget=forms.Textarea, label=COMMENT_LABEL)
 
 
 class OrganisationImportForm(forms.Form):
     file = forms.FileField()
-    comment = forms.CharField(widget=forms.Textarea)
+    comment = forms.CharField(widget=forms.Textarea, label=COMMENT_LABEL)
 
 
 class FundNewForm(forms.Form):
     id = forms.SlugField()
     name = forms.CharField()
-    comment = forms.CharField(widget=forms.Textarea)
+    comment = forms.CharField(widget=forms.Textarea, label=COMMENT_LABEL)
 
 
 class FundImportForm(forms.Form):
     file = forms.FileField()
-    comment = forms.CharField(widget=forms.Textarea)
+    comment = forms.CharField(widget=forms.Textarea, label=COMMENT_LABEL)
