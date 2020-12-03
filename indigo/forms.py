70b853f6f5a6b417435b5a4ac4a80bb889009b14
James <james.baster@opendataservices.coop>
2020-12-03 16:41:43 +0000

    ui: When creating projects, orgs or funds, check id is of right format

--- indigo/forms.py
+++ indigo/forms.py
@@ -1,10 +1,12 @@
 from django import forms
 
+from indigo.utils import validate_fund_id, validate_organisation_id, validate_project_id
+
 COMMENT_LABEL = "Comment for history"
 
 
 class ProjectNewForm(forms.Form):
-    id = forms.SlugField()
+    id = forms.SlugField(validators=[validate_project_id])
     name = forms.CharField()
     comment = forms.CharField(widget=forms.Textarea, label=COMMENT_LABEL)
 
@@ -26,7 +28,7 @@ class ProjectMakePrivateForm(forms.Form):
 
 
 class OrganisationNewForm(forms.Form):
-    id = forms.SlugField()
+    id = forms.SlugField(validators=[validate_organisation_id])
     name = forms.CharField()
     comment = forms.CharField(widget=forms.Textarea, label=COMMENT_LABEL)
 
@@ -37,7 +39,7 @@ class OrganisationImportForm(forms.Form):
 
 
 class FundNewForm(forms.Form):
-    id = forms.SlugField()
+    id = forms.SlugField(validators=[validate_fund_id])
     name = forms.CharField()
     comment = forms.CharField(widget=forms.Textarea, label=COMMENT_LABEL)
 
--- indigo/utils.py
+++ indigo/utils.py
@@ -1,4 +1,7 @@
+import re
+
 import openpyxl
+from django.core.exceptions import ValidationError
 
 
 def get_project_spreadsheet_version(filename):
@@ -16,3 +19,23 @@ def _get_spreadsheet_version(filename):
         if worksheet["A" + str(row_idx)].value == "SPREADSHEET VERSION":
             return worksheet["A" + str(row_idx + 1)].value
     return None
+
+
+def validate_project_id(value):
+    m = re.search("^INDIGO-POJ-[0-9][0-9][0-9][0-9]$", value)
+    if not m:
+        raise ValidationError("Project IDs should be of the format INDIGO-POJ-0000")
+
+
+def validate_fund_id(value):
+    m = re.search("^INDIGO-FUND-[0-9][0-9][0-9][0-9]$", value)
+    if not m:
+        raise ValidationError("Fund IDs should be of the format INDIGO-FUND-0000")
+
+
+def validate_organisation_id(value):
+    m = re.search("^INDIGO-ORG-[0-9][0-9][0-9][0-9]$", value)
+    if not m:
+        raise ValidationError(
+            "Organisation IDs should be of the format INDIGO-ORG-0000"
+        )
