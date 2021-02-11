b66e6423d3901e00102b7ad19b37162c8d8ef8ed
James <james.baster@opendataservices.coop>
2021-02-11 16:22:17 +0000

    spreadsheets: Fix bug in project spreadsheet around a ENUM option with a comma
    technical_assistance_details, approach

--- indigo/spreadsheetforms.py
+++ indigo/spreadsheetforms.py
@@ -21,6 +21,22 @@ def convert_project_data_to_spreadsheetforms_data(project, public_only=False):
     # Add ID
     data["id"] = project.public_id
 
+    # Cases where commas are in values can not be used in data validity options
+    # So replace them with a special value that we can use.
+    # This only applies in spreadsheets we edit; so not public ones
+    if not public_only:
+        # Fix Technical Assistance Details, Approach
+        for row in jsonpointer.resolve_pointer(
+            data, "/technical_assistance_details", []
+        ):
+            if (
+                jsonpointer.resolve_pointer(row, "/approach/value", "")
+                == "Trainings, Workshops and Boot Camps"
+            ):
+                jsonpointer.set_pointer(
+                    row, "/approach/value", "Trainings and Workshops and Boot Camps"
+                )
+
     # Done
     return data
 
@@ -44,6 +60,20 @@ def extract_edits_from_project_spreadsheet(record, import_json):
             # Remove Fund data from the data we save
             jsonpointer.set_pointer(
                 data_item, TYPE_PROJECT_FUND_LIST["item_key_with_fund_details"], None,
+            )
+
+    # Cases where commas are in values can not be used in data validity options
+    # So turn special values into values with commas
+    # Fix Technical Assistance Details, Approach
+    for row in jsonpointer.resolve_pointer(
+        import_json, "/technical_assistance_details", []
+    ):
+        if (
+            jsonpointer.resolve_pointer(row, "/approach/value", "")
+            == "Trainings and Workshops and Boot Camps"
+        ):
+            jsonpointer.set_pointer(
+                row, "/approach/value", "Trainings, Workshops and Boot Camps"
             )
 
     # Return
