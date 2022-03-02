678b67875c7f727bbfb2bfd895244106d408a464
James <james.baster@opendataservices.coop>
2022-03-02 17:33:25 +0000

    vagrant: Create Postgres user so Django tests work

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -28,6 +28,8 @@ pip3 install -r requirements_dev.txt  || true
 su --login -c "psql -c \"CREATE USER app WITH PASSWORD 'password' CREATEDB;\"" postgres
 su --login -c "psql -c \"CREATE DATABASE app WITH OWNER app ENCODING 'UTF8'  LC_COLLATE='en_GB.UTF-8' LC_CTYPE='en_GB.UTF-8'  TEMPLATE=template0 ;\"" postgres
 
+su --login -c "psql -c \"CREATE USER test WITH PASSWORD 'test' CREATEDB;\"" postgres
+
 
 echo "alias db='psql -U  app app  -hlocalhost'" >> /home/vagrant/.bashrc
 echo "localhost:5432:app:app:password" > /home/vagrant/.pgpass
