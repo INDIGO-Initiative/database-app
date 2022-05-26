cc2a4d2d3f1bdfffa1de848ad6542a0473c16c4a
Ben Webb <ben.webb@opendataservices.coop>
2022-05-26 10:17:53 +0100

    admin: Automatically set public ID when creating new records

--- indigo/tests/test_utils.py
+++ indigo/tests/test_utils.py
@@ -1,6 +1,5 @@
 import os
 
-from django.core.exceptions import ValidationError
 from django.test import TestCase  # noqa
 
 import indigo.utils
@@ -32,42 +31,3 @@ class GetOrganisationSpreadsheetVersion(TestCase):
         assert 2 == indigo.utils.get_organisation_spreadsheet_version(
             os.path.join(SPREADSHEET_DIR, "organisation_v002.xlsx")
         )
-
-
-class ValidateProjectId(TestCase):
-    def test_validate_project_id_1(self):
-        indigo.utils.validate_project_id("INDIGO-POJ-0001")
-
-    def test_validate_project_id_2(self):
-        with self.assertRaises(ValidationError):
-            indigo.utils.validate_project_id("INDIGO_POJ_0001")
-
-    def test_validate_project_id_3(self):
-        with self.assertRaises(ValidationError):
-            indigo.utils.validate_project_id("INDIGO-POJ-001")
-
-
-class ValidateFundId(TestCase):
-    def test_validate_fund_id_1(self):
-        indigo.utils.validate_fund_id("INDIGO-FUND-0001")
-
-    def test_validate_fund_id_2(self):
-        with self.assertRaises(ValidationError):
-            indigo.utils.validate_fund_id("INDIGO_FUND_0001")
-
-    def test_validate_fund_id_3(self):
-        with self.assertRaises(ValidationError):
-            indigo.utils.validate_fund_id("INDIGO-FUND-001")
-
-
-class ValidateOrganisationId(TestCase):
-    def test_validate_organisation_id_1(self):
-        indigo.utils.validate_organisation_id("INDIGO-ORG-0001")
-
-    def test_validate_organisation_id_2(self):
-        with self.assertRaises(ValidationError):
-            indigo.utils.validate_organisation_id("INDIGO_ORG_0001")
-
-    def test_validate_organisation_id_3(self):
-        with self.assertRaises(ValidationError):
-            indigo.utils.validate_organisation_id("INDIGO-ORG-001")
